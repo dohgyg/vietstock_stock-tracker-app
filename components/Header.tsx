b08528929c5028c8d91b0d9bfe3608de68cad90d
Phu Bui Viet <buivietphu123456@gmail.com>
2025-10-31 21:22:25 -0400

    Add another stock section

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,10 +8,11 @@ export const Header = () => {
     return (
         <header className="sticky top-0 header">
             <div className="container header-wrapper">
-                <Link href={"/"}>
+                <Link href={"/"} className="flex items-center space-x-3">
                     <Image src={"/assets/icons/logo.svg"} alt={"VietStock logo"} width={140} height={32}
                            className="h-8 w-auto cursor-pointer">
                     </Image>
+                    <p className="text-2xl font-bold text-white hover:text-yellow-500">Viet Stock</p>
                 </Link>
                 <nav className="hidden sm:block">
                     <NavItems/>
